dfc5f86ec3416bc382daebcfd99c117e3dbf77b6
nmbabaei <115838452+nmbabaei@users.noreply.github.com>
2023-10-06 19:49:09 -0400

    Comments

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -10,18 +10,24 @@ class Roll {
 
 }
 
+// Hardcoding roll objects
 let original = new Roll("Original", "Sugar milk", 1, 2.49);
 let walnut = new Roll("Walnut", "Vanilla milk", 12, 3.49);
 let raisin = new Roll("Raisin", "Sugar milk", 3, 2.99);
 let apple = new Roll("Apple", "Keep original", 3, 3.49);
 
+// Global variable to update price across functions
 let totalPrice = 0;
 
+// Create a set to store cart elements, add them all to DOM
 const cartSet = new Set([original, walnut, raisin, apple]);
 cartSet.forEach(addItem);
 
+
+// Function to add rolls to DOM
 function addItem(roll) {
 
+    // Access template, clone it, and connect it to the roll instance
     let template = document.querySelector(".cart-item");
     let clone = template.content.cloneNode(true);
     roll.element = clone.querySelector(".roll");
@@ -41,22 +47,19 @@ function addItem(roll) {
     let itemPrice = roll.element.querySelector(".cart-price");
     itemPrice.innerHTML = "$ " + price.toFixed(2);
 
+    // Update total price and display on page
     totalPrice += price;
     updateTotalPrice();
 
+    // Add clickability to the remove buttons
     roll.element.querySelector("#remove").addEventListener("click", () => {removeItem(roll)});
 
+    // Add this new cart element to DOM
     document.getElementById("cart-list").appendChild(roll.element);
 
 }
 
-function updateTotalPrice() {
-
-    let cartPrice = document.getElementById("total-price");
-    cartPrice.innerHTML = "$ " + Math.abs(totalPrice).toFixed(2);
-
-}
-
+// Function to remove items from cart
 function removeItem(roll) {
 
     // Remove from set
@@ -71,6 +74,7 @@ function removeItem(roll) {
 
 }
 
+// Function to calculate the price of an individual roll
 function calculatePrice(roll) {
 
     // For loop iterating through each glazing type, matching selected type to respective price
@@ -94,5 +98,13 @@ function calculatePrice(roll) {
     let price = ((roll.basePrice + selectedGlazing) * selectedPackSize).toFixed(2);
 
     return parseFloat(price);
+
+}
+
+// Function to update total price on the page
+function updateTotalPrice() {
+
+    let cartPrice = document.getElementById("total-price");
+    cartPrice.innerHTML = "$ " + Math.abs(totalPrice).toFixed(2);
 
 }
